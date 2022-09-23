e78b506fa55fa873ae76730b3e5bdee3ebceba45
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2022-09-23 10:35:55 +0700

    feat : eslintrs.js

--- vue-3-i18n-example-with-vue-i18n-next/src/components/HelloI18n.vue
+++ vue-3-i18n-example-with-vue-i18n-next/src/components/HelloI18n.vue
@@ -1,19 +1,26 @@
 <template>
-  <p>{{ $t("hello") }}</p>
+  <h1>{{ $t("title") }}</h1>
+  <p>{{ $t("description") }}</p>
+  <LocaleSwitcher />
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { useI18n } from "vue-i18n";
+import LocaleSwitcher from "../components/LocaleSwitcher.vue";
 
 export default defineComponent({
   name: "HelloI18n",
+  setup() {
+    const { t } = useI18n({
+      inheritLocale: true,
+      useScope: "local",
+    });
+
+    // Something todo ..
+
+    return { t };
+  },
+  components: { LocaleSwitcher },
 });
 </script>
-
-<i18n>
-{
-  "en": {
-    "hello": "Hello i18n in SFC!"
-  }
-}
-</i18n>
