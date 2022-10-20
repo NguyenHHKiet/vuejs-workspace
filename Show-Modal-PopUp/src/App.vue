46eadf5bbfef90d4a6f68a87740c21476490b163
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2022-10-20 16:28:13 +0700

    Show-Modal-PopUp

---
+++ Show-Modal-PopUp/src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import Modal from "./components/Modal.vue";
+export default {
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
+
+    <Teleport to="body">
+      <!-- use the modal component, pass in the prop -->
+      <Modal :show="showModal" @close="showModal = false">
+        <template #header>
+          <h3>custom header</h3>
+        </template>
+      </Modal>
+    </Teleport>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
