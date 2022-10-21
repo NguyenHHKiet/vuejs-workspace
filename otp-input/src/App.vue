03d8fd914aeca2414995c8de1f6dbad7ceb06d3e
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2022-10-21 14:36:08 +0700

    an OTP Input

---
+++ otp-input/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <otp :digit-count="4" @update:otp="otpValue = $event"></otp>
+
+    <p>The current OTP value is: {{ otpValue }}</p>
+  </div>
+</template>
+
+<script setup>
+import otp from "./components/OTP.vue";
+import { ref, onUpdated } from "vue";
+
+const otpValue = ref("");
+const value = ref();
+
+onUpdated(() => {
+  value.value = otpValue.value;
+  console.log(value.value);
+});
+</script>
+
+<style></style>
