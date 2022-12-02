9b586e56a9232019ceb29b6bfbea0d28fd6d336f
HoangKiet NguyenHuu <52524133+NguyenHHKiet@users.noreply.github.com>
2022-12-02 16:33:52 +0700

    tic tac toe

--- tic-tac-toe/src/App.vue
+++ tic-tac-toe/src/App.vue
@@ -1,44 +1,79 @@
-<script setup></script>
+<script setup>
+import { ref, computed } from 'vue';
+const player = ref('X');
+const board = ref([
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+]);
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (const item of lines) {
+    const [a, b, c] = item;
+    if (squares[a] && squares[b] === squares[a] && squares[c] === squares[a]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+const winner = computed(() => calculateWinner(board.value.flat()));
+const makeMove = (x, y) => {
+  if (winner.value) return;
+  if (board.value[x][y] !== '') return;
+  board.value[x][y] = player.value;
+  player.value = player.value === 'X' ? 'O' : 'X';
+};
+
+const resetGame = () => {
+  board.value = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
+  player.value = 'X';
+};
+</script>
 
 <template>
   <div>
-    <div class="grid grid-flow-col auto-cols-max">
-      <div>01</div>
-      <div>02</div>
-      <div>03</div>
+    <div class="pt-8 text-center dark:bg-gray-800 min-h-screen dark:text-white">
+      <h1 class="mb-8 text-3xl font-bold uppercase">Tic Tac Toe</h1>
+      <h3 class="text-xl mb-4">Player {{ player }}'s turn'</h3>
+
+      <div class="flex flex-col items-center mb-8">
+        <div v-for="(row, x) in board" :key="x" class="flex">
+          <div
+            v-for="(cell, y) in row"
+            :key="y"
+            @click="makeMove(x, y)"
+            :class="`material-icons-outlined border border-white w-20 h-20 hover:bg-gray-400 flex items-center justify-center text-4xl cursor-pointer ${
+              cell === 'X' ? 'text-pink-500' : 'text-blue-500'
+            }`"
+          >
+            {{ cell === 'X' ? 'close' : cell === 'O' ? 'circle' : '' }}
+          </div>
+        </div>
+      </div>
+      <h2 v-if="winner" class="text-6xl font-bold mb-8">
+        Player '{{ winner }}' winner!!
+      </h2>
+      <button
+        @click="resetGame"
+        class="px-4 py-2 bg-pink-500 rounded uppercase font-bold hover:bg-pink-700 duration-300"
+      >
+        Reset Game
+      </button>
     </div>
-    <button class="outline outline-2 outline-offset-2 ...">Button A</button>
-    <button class="outline-dashed outline-2 outline-offset-2 ...">
-      Button B
-    </button>
-    <button class="outline-dotted outline-2 outline-offset-2 ...">
-      Button C
-    </button>
-    <button class="outline-double outline-3 outline-offset-2 ...">
-      Button D
-    </button>
-    <table class="border-collapse border border-slate-500 ...">
-      <thead>
-        <tr>
-          <th class="border border-slate-600 ...">State</th>
-          <th class="border border-slate-600 ...">City</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td class="border border-slate-700 ...">Indiana</td>
-          <td class="border border-slate-700 ...">Indianapolis</td>
-        </tr>
-        <tr>
-          <td class="border border-slate-700 ...">Ohio</td>
-          <td class="border border-slate-700 ...">Columbus</td>
-        </tr>
-        <tr>
-          <td class="border border-slate-700 ...">Michigan</td>
-          <td class="border border-slate-700 ...">Detroit</td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
