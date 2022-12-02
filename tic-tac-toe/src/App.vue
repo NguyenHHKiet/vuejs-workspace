ba6048c0a062a60f05f9afd6cbb4ff64c05a524d
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2022-12-02 15:16:19 +0700

    intial project

---
+++ tic-tac-toe/src/App.vue
@@ -0,0 +1,45 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div class="grid grid-flow-col auto-cols-max">
+      <div>01</div>
+      <div>02</div>
+      <div>03</div>
+    </div>
+    <button class="outline outline-2 outline-offset-2 ...">Button A</button>
+    <button class="outline-dashed outline-2 outline-offset-2 ...">
+      Button B
+    </button>
+    <button class="outline-dotted outline-2 outline-offset-2 ...">
+      Button C
+    </button>
+    <button class="outline-double outline-3 outline-offset-2 ...">
+      Button D
+    </button>
+    <table class="border-collapse border border-slate-500 ...">
+      <thead>
+        <tr>
+          <th class="border border-slate-600 ...">State</th>
+          <th class="border border-slate-600 ...">City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="border border-slate-700 ...">Indiana</td>
+          <td class="border border-slate-700 ...">Indianapolis</td>
+        </tr>
+        <tr>
+          <td class="border border-slate-700 ...">Ohio</td>
+          <td class="border border-slate-700 ...">Columbus</td>
+        </tr>
+        <tr>
+          <td class="border border-slate-700 ...">Michigan</td>
+          <td class="border border-slate-700 ...">Detroit</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped></style>
